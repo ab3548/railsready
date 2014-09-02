be364c256fe6ace39436c3969cd52ea347a0e46a
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-09-02 10:55:48 +0200

    install from ubuntu sources

--- railsready.sh
+++ railsready.sh
@@ -18,10 +18,6 @@ sudo locale-gen en_GB.UTF-8
 passenger-install-apache2-module --auto
 
 #install mongo
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-sudo apt-get update
-sudo apt-get -y install mongodb-org
-sudo service mongod start
+sudo apt-get -y install mongodb
 sudo apt-get -y install mongodb-clients
 
