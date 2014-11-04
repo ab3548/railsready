687f05a94e795cd4c46945edcada41f6439b2ea6
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-11-04 09:45:47 +0100

    Update railsready.sh

--- railsready.sh
+++ railsready.sh
@@ -25,7 +25,6 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
 sudo apt-get install mongodb-10gen
-sudo apt-get install mongodb-10gen=2.4.12
 echo "mongodb-10gen hold" | sudo dpkg --set-selections
 sudo service mongodb start
 sudo apt-get -y install mongodb-clients
