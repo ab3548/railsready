0347ea453cc6e72ed65af21f805145df097390d2
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-11-29 10:25:11 +0100

    Update railsready.sh

--- railsready.sh
+++ railsready.sh
@@ -1,4 +1,7 @@
-sudo -i
+dd if=/dev/zero of=/swap bs=1M count=1024
+sudo mkswap /swap
+sudo swapon /swap
+
 sudo apt-get -y update
 sudo apt-get -y install zlib1g-dev libreadline6-dev libyaml-dev ruby-dev
 sudo apt-get -y install wget curl build-essential clang bison openssl zlib1g libxslt1.1 libssl-dev libxslt1-dev libxml2 libffi-dev libyaml-dev libxslt-dev autoconf libc6-dev libreadline6-dev zlib1g-dev libcurl4-openssl-dev libtool
@@ -8,7 +11,6 @@ apt-get -y install apache2-mpm-worker
 apt-get -y install apache2-threaded-dev
 apt-get -y install libapr1-dev
 apt-get -y install libaprutil1-dev
-sudo apt-get -y install libssl-dev
 cd /tmp
 wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz
 tar -xvzf ruby-2.1.2.tar.gz
